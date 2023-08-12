dad7064d6286314ee0c70378e96e7345d441c187
sapey <alexandre.froment92@gmail.com>
2023-08-12 02:10:46 +0200

    add some style to the thread

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
     id: string;
     currentUserId: string;
@@ -33,10 +36,37 @@ const ThreadCard = ({
     comments,
 }: Props) => {
     return (
-        <article>
-            <h2 className="text-small-regular text-light-2">
-                {content}
-            </h2>
+        <article className="flex w-full flex-col rounded-x1 bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-rows gap-4">
+                    <div className="flex flex-col items-center">
+                        <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                            <Image
+                                src={author.image}
+                                alt="Profile image"
+                                fill
+                                className="cursor-pointer"
+                            />
+                        </Link>
+
+                        <div className="thread-card_bar" />
+                    </div>
+
+                    <div className="flex w-full flex-col">
+                        <Link href={`/profile/${author.id}`} className="w-fit">
+                            <h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4>
+                        </Link>
+
+                        <p className="mt-2 text-small-regumar text-light-2">{content}</p>
+
+                        <div className="mt-5 flex flex-col gap-3">
+                            <div className="flex gap-3.5">
+                                <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} className="cursor-pointer object-contain"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </article>
     )
 }
